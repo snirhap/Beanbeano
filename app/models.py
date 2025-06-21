0436e77009487c73db85e8cfebe2bf5a7bac6b44
snirhap <snirhap@gmail.com>
2025-06-21 23:37:17 +0300

    add Roaster and Bean db models

--- app/models.py
+++ app/models.py
@@ -6,3 +6,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    # role = ...
+
+
+class Roaster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    beans = db.relationship('Bean', backref='roaster', lazy=True)
+
+class Bean(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    roast_level = db.Column(db.String(150), nullable=False)
+    origin = db.Column(db.String(150), nullable=False)
+    price_per_100_grams = db.Column(db.Float(150), nullable=False)
+    roaster_id = db.Column(db.Integer, db.ForeignKey('roaster.id'), nullable=False)
