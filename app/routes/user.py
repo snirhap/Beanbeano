e137a766b9f99e0ef2b08aa6c6bf2daecc8584ff
Snir Ohayon <snirhap@gmail.com>
2025-07-27 16:26:22 +0300

    Add user's favorite brew methods endpoint

--- app/routes/user.py
+++ app/routes/user.py
@@ -15,7 +15,7 @@ def fetch_user_with_reviews(user_id):
 def get_user(user_id):
     user, _ = fetch_user_with_reviews(user_id)
     if not user:
-        return jsonify({"message": "User not exist"}), 404
+        return jsonify({"message": "User does not exist"}), 404
     return jsonify({"message": user.to_dict()}), 200
     
 @user_bp.route('/users/<int:user_id>/reviews', methods=['GET'])
@@ -23,5 +23,17 @@ def get_user_reviews(user_id):
     with current_app.db_manager.get_read_session() as session:
         user, reviews = fetch_user_with_reviews(user_id)
         if not user:
-            return jsonify({"message": "User not exist"}), 404
+            return jsonify({"message": "User does not exist"}), 404
         return jsonify({"message": [r.to_dict() for r in reviews]}), 200
+
+@user_bp.route('/users/<int:user_id>/brew_methods', methods=['GET'])
+def favorite_brew_methods(user_id):    
+    user, reviews = fetch_user_with_reviews(user_id)
+    brew_methods = dict()
+    if not user:
+        return jsonify({"message": "User does not exist"}), 404
+    
+    for r in reviews:
+        brew_methods[r.brew_method] = brew_methods.get(r.brew_method, 0) + 1
+
+    return jsonify({"brew_methods": dict(sorted(brew_methods.items(), key=lambda x: x[1], reverse=True))}), 200
